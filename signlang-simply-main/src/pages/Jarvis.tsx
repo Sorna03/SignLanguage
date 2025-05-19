3b3e8955ec34124e2a879f4868d64a7ec47fbc1b
Sorna03 <sornaramanathan3@gmail.com>
2025-05-19 21:32:02 +0530

    Initial commit

---
+++ signlang-simply-main/src/pages/Jarvis.tsx
@@ -0,0 +1,15 @@
+const Jarvis = () => {
+  return (
+    <div className="min-h-screen p-4">
+      <iframe
+        src="http://localhost:3001/"
+        title="Jarvis Assistant"
+        width="100%"
+        height="1000px"
+        frameBorder="0"
+      />
+    </div>
+  );
+};
+
+export default Jarvis;
